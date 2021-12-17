dde6eac92ec3d571cbc4fd59c0713f31e68f022d
johackim <contact@johackim.com>
2021-12-17 19:33:25 +0100

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,17 @@
 module.exports = {
     stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-tailwind-dark-mode'],
+    addons: [
+        'storybook-tailwind-dark-mode',
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss'),
+                },
+            },
+        },
+    ],
     framework: '@storybook/react',
 };
