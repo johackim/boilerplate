49b1393e47852ee2c225cbffce34b1ed9d4dbc4d
johackim <contact@johackim.com>
2022-03-27 13:04:14 +0200

    Update hero

--- components/hero.js
+++ components/hero.js
@@ -1,28 +1,30 @@
 import Button from '@components/button';
 
-const Hero = ({ preHead, headline, subHeadline, callToAction, secondaryAction }) => (
-    <section className="h-screen flex justify-center text-center items-center mx-auto">
+const Hero = ({ children, preHead, headline, subHeadline, centered }) => (
+    <section className={`${centered && 'h-screen'} flex justify-center text-center items-center mx-auto`}>
         <div className="px-2 sm:px-0">
             {preHead && <h3 className="text-sm lg:text-lg my-4">{preHead}</h3>}
 
-            { headline && <h2 className="mx-auto text-3xl max-w-2xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">{headline}</h2> }
+            { headline && <h1 className="mx-auto text-3xl max-w-2xl tracking-tight leading-10 font-medium text-gray-700 dark:text-gray-300 sm:text-5xl sm:leading-none md:text-6xl">{headline}</h1> }
 
-            { subHeadline && <h3 className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">{subHeadline}</h3> }
+            { subHeadline && <h2 className="max-w-md mx-auto text-base text-gray-500 dark:text-gray-400 sm:text-lg md:text-xl md:max-w-3xl">{subHeadline}</h2> }
 
             <div className="mt-4 grid gap-2 sm:grid-flow-col sm:auto-cols-max sm:justify-center">
-                { callToAction && <Button path={callToAction.path} label={callToAction.name} /> }
-                { secondaryAction && <Button path={secondaryAction.path} label={secondaryAction.name} primary={false} /> }
+                {children}
             </div>
         </div>
     </section>
 );
 
 Hero.defaultProps = {
+    centered: true,
     preHead: '',
     headline: 'Headline',
     subHeadline: 'Sub-headline',
-    callToAction: { name: 'Call to action', path: '/' },
-    secondaryAction: { name: 'Secondary action', path: '/' },
+    children: [
+        <Button key="main" href="/">Call To Action</Button>,
+        <Button key="secondary" href="/" secondary>Secondary Action</Button>,
+    ],
 };
 
 export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from '@components/layout';
 import Hero from '@components/hero';
 
-const Page = () => <Hero />;
+const Page = () => <Hero centered={false} />;
 
 Page.getLayout = (page) => <Layout className="flex flex-col justify-center">{page}</Layout>;
 
